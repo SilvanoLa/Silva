6b93c001bdf0db57e0ad763e9aace4c5d9bc6fe4
Silvano Lattes <silvanolattes@gmail.com>
2023-03-08 10:51:28 -0300

    Primera escritura en el repositorio

---
+++ js/funciones.js
@@ -0,0 +1,19 @@
+//
+
+/**
+ * 
+ * 
+ */
+
+document.getElementById("mostrar_foto").onclick = function(){
+    document.getElementById("fotoperfil").style.display = "block";
+}
+
+document.getElementById("nombre_gato").onclick = function(){
+    document.getElementById("Titu").innerHTML = "Mi nombre es Titu";
+ }
+
+
+
+
+        
