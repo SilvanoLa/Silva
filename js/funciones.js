435b3cb418f7653b19fafe838cbfca020c034437
Silvano Lattes <silvanolattes@gmail.com>
2023-03-08 21:55:17 -0300

    Realice algunas modificaciones

--- js/funciones.js
+++ js/funciones.js
@@ -7,10 +7,14 @@
 
 document.getElementById("mostrar_foto").onclick = function(){
     document.getElementById("fotoperfil").style.display = "block";
+    document.getElementById("camara").style.display = "none";
+    document.getElementById("mostrar_foto").style.display = "none";
 }
 
 document.getElementById("nombre_gato").onclick = function(){
     document.getElementById("Titu").innerHTML = "Mi nombre es Titu";
+    document.getElementById("Titu").style.display = "block";
+    document.getElementById("nombre_gato").style.display = "none";
  }
 
 
